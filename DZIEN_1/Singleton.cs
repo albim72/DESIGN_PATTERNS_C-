8c9ab866aa921dd77e5df68b7753b27ce6282f03
albim72 <marcin.albiniak72@gmail.com>
2026-06-15 13:44:01 +0200

    Update Singleton.cs

--- DZIEN_1/Singleton.cs
+++ DZIEN_1/Singleton.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Singleton.DocumentGenerator;
 
 //Singleton użyty jako jedna istancja klasy konfigurującej system, która jest używana w wielu miejscach w aplikacji. Singleton zapewnia, że istnieje tylko jedna instancja tej klasy i umożliwia globalny dostęp do niej.
 
@@ -51,15 +52,51 @@ namespace Singleton
         }
        public void GenerateDecision(string caseNumber)
         {
+            AppConfiguration configuration = AppConfiguration.Instance;
             // Logika generowania decyzji na podstawie numeru sprawy
-            Console.WriteLine($"Generowanie decyzji dla sprawy: {caseNumber}");
+            Console.WriteLine($"Generowanie decyzji administracyjnej dla sprawy: {caseNumber}");
+            Console.WriteLine($"Używana aplikacja: {configuration.ApplicationName}");
+            Console.WriteLine($"Używany format dokumentu: {configuration.DefaultDocumentFormat}");
+            Console.WriteLine($"Środowisko: {configuration.EnvironmentName}");
         }
     }
 
     internal class Program
     {
+  
         static void Main(string[] args)
         {
+            Console.WriteLine("Tworzenie instancji DocumentGenerator...WZORZEC SINGLETON");
+            Console.WriteLine("________________________________________________");
+            AppConfiguration config1 = AppConfiguration.Instance;
+            AppConfiguration config2 = AppConfiguration.Instance;
+
+            Console.WriteLine($"Czy config1 i config2 to ta sama instancja? {ReferenceEquals(config1, config2)}");
+
+            config1.PrintConfiguration();
+            Console.WriteLine("_________________________________");
+            config2.PrintConfiguration();
+
+
+            Console.WriteLine("Zmiana środowiska na 'PROD' za pomocą config1...");
+            config1.ChangeEnvironment("PROD");
+
+            config2.PrintConfiguration();
+
+            //generowanie dokumentu decyzji administracyjnej
+            var generator = new DocumentGenerator();
+            generator.GenerateDecision("ZUS/12345/2024");
+
+            Console.WriteLine(generator.GetType().Name);
+
+            Console.WriteLine("Singleton Test");
+
+            SingletonTest singletonTest1 = SingletonTest.Instance;
+            SingletonTest singletonTest2 = SingletonTest.Instance;
+
+            Console.WriteLine($"Czy singletonTest1 i singletonTest2 to ta sama instancja? {ReferenceEquals(singletonTest1, singletonTest2)}");
+
+
         }
     }
 }
